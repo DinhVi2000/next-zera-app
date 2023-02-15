42b351a4d0c31b67ae3f7b9e30b6382a847a7f9f
vyntt <vyntt@stisolutions.net>
2023-02-15 09:46:05 +0700

    [Register, Profile] Change toast, update main layout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Footer from "@/components/ui/Footer";
+import TopBar from '@/components/ui/TopBar';
 
 const MainLayout = ({ children }) => {
   return (
     <div>
-      {children}
+      <div className="flex justify-between gap-4 p-4 w-fit mx-auto relative">
+        <TopBar />
+        <div className="ml-[222px]">{children}</div>
+      </div>
       <Footer></Footer>
     </div>
   );
