aac7a4c7fbb3eef25ec3a5bf3ce9fd7d1052e1cc
hoangnt <hoangnt@stisolutions.com>
2023-02-16 14:46:40 +0700

    Dockerfile

--- ops/push.sh
+++ ops/push.sh
@@ -3,21 +3,22 @@
 set -xeo pipefail
 [[ -f "aws-envs.sh" ]] && source aws-envs.sh
 
-BACKEND_IMAGE_NAME="nuwton_fe_market"
+GAME_IMAGE_NAME="portal-fe"
 BRANCH_NAME=$(echo "$CI_COMMIT_BRANCH" | sed -r "s,/,-,g")
 AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID}"
-ECR_URL="${AWS_ACCOUNT_ID}.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/nuwton-$AWS_ENV-repo-fe-market"
+ECR_URL="${AWS_ACCOUNT_ID}.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/zera-$AWS_ENV-repo-user-web"
 
 build_image(){
-    docker image build -f Dockerfile -t "${BACKEND_IMAGE_NAME}:latest" --build-arg NODE_ENV=$AWS_ENV .
+    docker image build -f Dockerfile -t "${GAME_IMAGE_NAME}:latest" --build-arg NODE_ENV=$AWS_ENV .
 }
 
 push_image() {
-    aws ecr get-login-password --region $AWS_DEFAULT_REGION --profile nuwton-$AWS_ENV | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-    docker image tag "${BACKEND_IMAGE_NAME}:latest" "${ECR_URL}:${image_tag}"
+    # aws ecr get-login-password --region $AWS_DEFAULT_REGION --profile zera-$AWS_ENV | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.zera.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+    aws ecr get-login-password --region $AWS_DEFAULT_REGION --profile zera-$AWS_ENV | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+    docker image tag "${GAME_IMAGE_NAME}:latest" "${ECR_URL}:${image_tag}"
     docker image push "${ECR_URL}:${image_tag}" &
-    fe_process=$! && echo $fe_process
-    wait $fe_process
+    game_process=$! && echo $game_process
+    wait $game_process
 }
 
 build_image
@@ -30,5 +31,5 @@ export IMAGE_TAG="${image_tag}"
 export AWS_ACCOUNT_ID="$AWS_ACCOUNT_ID"
 export ECR_URL="$ECR_URL"
 export AWS_REGION="$AWS_REGION"
-export BACKEND_IMAGE_NAME="$BACKEND_IMAGE_NAME"
+export GAME_IMAGE_NAME="$GAME_IMAGE_NAME"
 EOT
