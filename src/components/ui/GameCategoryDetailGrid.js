2869617da143ecb968c95edb79c058e9b0079b81
vind <vindde140058@fpt.edu.vn>
2023-02-28 17:12:33 +0700

    [game-category-detail] implement api

--- src/components/ui/GameCategoryDetailGrid.js
+++ src/components/ui/GameCategoryDetailGrid.js
@@ -2,13 +2,17 @@ import { GAMES_IMAGES } from "@/utils/constant";
 import { getRandom } from "@/utils/helper";
 import React from "react";
 import { useSelector } from "react-redux";
+import GameCategory from "../game/GameCategory";
 import GameItem from "../game/GameItem";
 
 const GameCategoryDetailGrid = () => {
   const { categoryDetail } = useSelector(({ game }) => game) ?? {};
 
+  const { game_category, other_category } = categoryDetail ?? {};
+  const { game_detail, label } = game_category ?? {};
+
   return (
-    <div className="grid-category-detail">
+    <div className="grid-category-detail transition-all">
       <div
         style={{
           gridArea: "ct / ct / ct / ct",
@@ -17,34 +21,112 @@ const GameCategoryDetailGrid = () => {
         }}
         className="text-white min-h-[94px] rounded-2xl flex items-center justify-center text-lg font-bold"
       >
-        App Store games
+        {label}
       </div>
 
       {/* game */}
-      {Array(10)
-        .fill(0)
-        ?.map((e, i) => (
+      {game_detail?.length > 0 &&
+        game_detail?.map((e, i) => (
           <GameItem
             key={i}
+            id={e?.id}
+            index={i}
+            thumbnail={e?.thumbnail}
+            title={e?.title}
             style={{
-              gridArea: `g${i + 1} / g${i + 1} / g${i + 1} / g${i + 1}`,
+              gridArea:
+                i < 8 ? `g${i + 1} / g${i + 1} / g${i + 1} / g${i + 1}` : "",
             }}
           />
         ))}
 
-      {/* {Array(10)
+      {Array(40 - game_detail?.length || 0)
         .fill(0)
         ?.map((e, i) => (
-          <div
-            key={i}
+          <GameItem
+            key={game_detail?.length + i}
+            id={game_detail?.length + i}
+            index={game_detail?.length + i}
+            thumbnail={getRandom(GAMES_IMAGES)}
+            title={`game ${game_detail?.length + i}`}
             style={{
-              gridRowStart: "span 2",
-              gridColumnStart: "span 2",
+              gridArea:
+                game_detail?.length + i < 8
+                  ? `g${game_detail?.length + i + 1} / g${
+                      game_detail?.length + i + 1
+                    } / g${game_detail?.length + i + 1} / g${
+                      game_detail?.length + i + 1
+                    }`
+                  : "",
             }}
-            className="bg-white min-h-[94px] rounded-2xl"
-          ></div>
-        ))} */}
+          ></GameItem>
+        ))}
+
+      {!game_detail &&
+        Array(40)
+          .fill(0)
+          .map((e, i) => (
+            <ItemLoading
+              key={i}
+              style={{
+                gridArea:
+                  i < 8 ? `g${i + 1} / g${i + 1} / g${i + 1} / g${i + 1}` : "",
+              }}
+            ></ItemLoading>
+          ))}
+
+      {/* category */}
+      {other_category?.length > 0 &&
+        other_category?.map((e, i) => (
+          <GameCategory
+            key={i}
+            id={e?.id}
+            index={i + 7}
+            label={e?.label}
+            thumbnail={e?.thumbnail}
+            style={{
+              gridArea: `c${i + 1} / c${i + 1} / c${i + 1} / c${i + 1}`,
+            }}
+          />
+        ))}
+
+      {Array(4 - other_category?.length || 0)
+        .fill(0)
+        ?.map((e, i) => (
+          <GameCategory
+            key={other_category?.length + i}
+            id={other_category?.length + e?.id}
+            index={other_category?.length + i + 7}
+            label={"label"}
+            thumbnail={getRandom(GAMES_IMAGES)}
+            style={{
+              gridArea: `c${other_category?.length + i + 1} / c${
+                other_category?.length + i + 1
+              } / c${other_category?.length + i + 1} / c${
+                other_category?.length + i + 1
+              }`,
+            }}
+          />
+        ))}
+
+      {!other_category &&
+        Array(4)
+          .fill(0)
+          .map((e, i) => (
+            <ItemLoading
+              key={i}
+              style={{
+                gridArea: `c${i + 1} / c${i + 1} / c${i + 1} / c${i + 1}`,
+              }}
+            ></ItemLoading>
+          ))}
     </div>
+  );
+};
+
+const ItemLoading = ({ ...props }) => {
+  return (
+    <div {...props} className="skeleton-shine w-full h-full rounded-2xl"></div>
   );
 };
 
--- src/components/ui/GameGrid.js
+++ src/components/ui/GameGrid.js
@@ -10,11 +10,6 @@ const GameGrid = ({ games }) => {
   return (
     <>
       <div className="game-grid">
-        {/* {Array(30)
-          .fill(0)
-          .map((e, i) => (
-            <GameItem key={i} size={1}></GameItem>
-          ))} */}
         {games?.map((e, i) => (
           <GameItem
             key={e?.id}
