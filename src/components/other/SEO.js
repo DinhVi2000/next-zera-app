3641048d724491cc7ae71e2a8a3b97e6c18f961f
vyntt <vyntt@stisolutions.net>
2023-04-10 11:06:40 +0700

    [Meta tags] Fix meta tags

--- src/components/other/SEO.js
+++ src/components/other/SEO.js
@@ -6,33 +6,16 @@ const SEO = ({ title, description }) => {
     <Head>
       <title>{title || "Online Games on Zera"}</title>
       <meta name="title" content={title || "Online Games on Zera"}></meta>
-      <meta name="og:description" content={description} />
-      <meta name="description" content={description} />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, user-scalable=no"
       />
-      <meta
-        property="og:image"
-        content="../../../public/images/thumbnail.png"
-      ></meta>
-      <meta
-        property="image"
-        content="../../../public/images/thumbnail.png"
-      ></meta>
-      <link rel="icon" href="/favicon.ico" />
 
-      <meta
-        name="twitter:title"
-        content={title || "Online Games on Zera"}
-        key="tw-title"
-      />
-      <meta name="twitter:description" content={description} key="tw-desc" />
-      <meta
-        name="twitter:image"
-        content="../../../public/images/thumbnail.png"
-        key="tw-image"
-      />
+      <meta name="description" content={description} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:title" content={title} />
     </Head>
   );
 };
