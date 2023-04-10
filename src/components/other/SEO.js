b522cc102250c4c78983d2bfffd3d151113375f8
vyntt <vyntt@stisolutions.net>
2023-04-10 14:23:53 +0700

    [SEO] Fix meta tags

--- src/components/other/SEO.js
+++ src/components/other/SEO.js
@@ -4,14 +4,19 @@ import React from "react";
 const SEO = ({ title, description }) => {
   return (
     <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, user-scalable=no"
+      />
+
       <title>{title || "Online Games on Zera"}</title>
-      <meta name="title" content={title || "Online Games on Zera"} />
-      <meta name="description" content={description} />
+      <meta name="title" content="Online Games on Zera" />
+      <meta name="description" content="description" />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://user-web.test.rivennft.io/" />
-      <meta property="og:title" content={title || "Online Games on Zera"} />
-      <meta property="og:description" content={description} />
+      <meta property="og:title" content="Online Games on Zera" />
+      <meta property="og:description" content="description" />
       <meta
         property="og:image"
         content="https://user-web.test.rivennft.io/images/thumbnail.png"
@@ -22,11 +27,8 @@ const SEO = ({ title, description }) => {
         property="twitter:url"
         content="https://user-web.test.rivennft.io/"
       />
-      <meta
-        property="twitter:title"
-        content={title || "Online Games on Zera"}
-      />
-      <meta property="twitter:description" content={description} />
+      <meta property="twitter:title" content="Online Games on Zera" />
+      <meta property="twitter:description" content="description" />
       <meta
         property="twitter:image"
         content="https://user-web.test.rivennft.io/images/thumbnail.png"
